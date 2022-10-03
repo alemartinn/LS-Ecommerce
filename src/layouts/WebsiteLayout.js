7576c83ba2edfd7d1cb69448af3b289f8fdb539c
juanPedroGalarza <juanpg232@gmail.com>
2022-10-03 16:35:21 -0300

    basics commponents

---
+++ src/layouts/WebsiteLayout.js
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+
+function WebsiteLayout(props) {
+
+    return (
+        <>
+            <Header />
+            {props.children}
+            <Footer />
+        </>
+    )
+
+}
+
+export default WebsiteLayout
