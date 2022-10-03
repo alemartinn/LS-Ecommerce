028c85d2537bd18d9d120c70bac6220f4b9b0f8a
juanPedroGalarza <juanpg232@gmail.com>
2022-10-03 19:45:51 -0300

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,62 @@
 import { useSelector } from "react-redux";
-
+import styled from "styled-components"
+import {Link as LinkR} from "react-router-dom"
 function Footer() {
 
     const { bcgColor, fontColor } = useSelector(state => state.color)
 
     return (
-        <footer style={{backgroundColor: bcgColor}}>
-            <h3 style={{color: fontColor}}>Footer</h3>
-        </footer>
+        <FooterContainer bcgColor={bcgColor}>
+        <FooterContent>
+            <ColFooter>
+                <TextFooter color={fontColor}>Social Media...</TextFooter>
+                <TextFooter color={fontColor}>©2022 LS Food Co</TextFooter>
+            </ColFooter>
+            <ColFooter>
+                    <TextFooter color={fontColor}>LOGO</TextFooter>
+                    <p style={{color:fontColor}}>
+                    <LinkRouter to={"/terms"}
+                            textColor={fontColor}>Terms </LinkRouter>
+                        /
+                    <LinkRouter to={"/about"}
+                        textColor={fontColor}> About Us</LinkRouter>
+                    </p>
+            </ColFooter>
+            <ColFooter>
+                <TextFooter color={fontColor}>Footer nav</TextFooter>
+            </ColFooter>
+        </FooterContent>
+        </FooterContainer>
     )
 }
-
+//styles
+const FooterContainer = styled.footer`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    background-color:${props=>props.bcgColor};
+    padding: 0 2rem;
+`
+const FooterContent = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-width: 1400px;
+    min-height: 20vh;
+`
+const TextFooter = styled.p`
+    color: ${props => props.color};
+    margin: 0;
+`
+const ColFooter = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+`
+const LinkRouter = styled(LinkR)`
+    text-decoration: none;
+    color: ${props=> props.textColor}
+`
 export default Footer
