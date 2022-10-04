a6da8adbba7f7b5719e0e8901110b9e31238b84b
Elvio Galeano <elvio.galeano827@gmail.com>
2022-10-04 19:40:53 -0300

    pagina de details

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import HIW from './pages/HIW';
+import Detail from './pages/Detail';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/hiw' element={<HIW />} />
           <Route path='/*' element={<NotFound />} />
+          <Route path='/detail' element={<Detail />} />
         </Routes>
       </WebsiteLayout>
     </BrowserRouter>
