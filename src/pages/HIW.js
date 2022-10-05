9326fb1a5a3873954bc82566b39b2562fd630a54
juanPedroGalarza <juanpg232@gmail.com>
2022-10-05 15:11:29 -0300

    HIW styles

--- src/pages/HIW.js
+++ src/pages/HIW.js
@@ -1,89 +1,36 @@
 import { useSelector } from "react-redux";
-import { Link as LinkR } from "react-router-dom";
-import styled from "styled-components"
+import "../styles/HIW.css"
 
 function HIW() {
 
     const { bcgColor, fontColor, thirdColor, fourthColor, fivethColor } = useSelector(state => state.color)
     return (
-        <MainHIW style={{ backgroundColor: bcgColor }}>
-            <MainContent>
-                <Guide bcgColor={fivethColor}>
-                    <TextP textcolor={"#ccc"} >Steps guide</TextP>
-                </Guide>
-                <Promo>
+        <main className="hiw-main" style={{ backgroundColor: bcgColor }}>
+            <div className="hiw-content">
+                <div className="hiw-guide"
+                    style={{ backgroundColor: fivethColor }}>
+                    <p className="hiw-text"
+                        style={{ color: "#ccc" }} >Steps guide</p>
+                </div>
+                <div className="hiw-promo">
                     <div style={{ height: 100, width: 100, backgroundColor: thirdColor }}></div>
-                    <TextP textcolor={fontColor} >Mobile App Promo</TextP>
-                </Promo>
-                <Benefits bcgColor={fourthColor}>
-                    <TextP textcolor={fontColor} >Benefits</TextP>
-                </Benefits>
-                <FAQ bcgColor={fourthColor}>
-                    <TextP textcolor={fontColor} >FAQ</TextP>
-                </FAQ>
-            </MainContent>
-        </MainHIW>
+                    <p className="hiw-text"
+                        style={{ color: fontColor }} >Mobile App Promo</p>
+                </div>
+                <div className="hiw-benefits"
+                    style={{ backgroundColor: fourthColor }}>
+                    <p className="hiw-text"
+                    style={{ color: fontColor }} >Benefits</p>
+                </div>
+                <div className="hiw-faq" 
+                style={{ backgroundColor: fourthColor }}>
+                    <p className="hiw-text"
+                    style={{ color: fontColor }} >FAQ</p>
+                </div>
+            </div>
+        </main>
     )
 }
-const MainHIW = styled.main`
-    display: flex;
-    justify-content: center;
-    width: 100%;
-    min-height: 80vh;
-`
-const MainContent = styled.div`
-    width: 100%;
-    max-width: 1400px;
-    padding-top: 10vh ;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-` 
-const Guide = styled.div`
-    width: 100%;
-    max-width: 1000px;
-    min-height: 250px;
-    border-radius: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    margin-bottom: 5vh;
-    background-color: ${props=>props.bcgColor}
-`
-const Benefits = styled.div`
-    width: 100%;
-    max-width: 1000px;
-    min-height: 250px;
-    border-radius: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    margin-bottom: 5vh;
-    background-color: ${props=>props.bcgColor}
-`
-const FAQ = styled.div`
-    width: 100%;
-    max-width: 1000px;
-    min-height: 250px;
-    border-radius: 20px;
-    display: flex;
-    flex-wrap: wrap;
-    margin-bottom: 5vh;
-    background-color: ${props=>props.bcgColor}
-`
-const Promo = styled.div`
-    width: 100%;
-    max-width: 1000px;
-    min-height: 250px;
-    border-radius: 20px;
-    margin-bottom: 5vh;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-`
 
-const TextP = styled(LinkR)`
-    color: ${props => props.textcolor};
-    text-align: center;
-    font-size: 1rem;
-`
 
 export default HIW
