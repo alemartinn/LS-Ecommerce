82ebd0d66aa7986dc5f5d43b70f144f678526d50
unknown <kavellid63@gmail.com>
2022-10-06 01:00:07 -0300

    import de Modal pasado de App a WebsiteLayout

--- src/layouts/WebsiteLayout.js
+++ src/layouts/WebsiteLayout.js
@@ -1,17 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux'
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-
+import Modal from '../components/Modal'
+import FormSignIn from '../components/FormSignIn';
+import ModalCard from '../components/ModalCard';
 
 function WebsiteLayout(props) {
-
-    return (
-        <>
-            <Header />
-            {props.children}
-            <Footer />
-        </>
-    )
-
+  const { isOpen, modalType } = useSelector((store) => store.modal)
+  const showModal = () => {
+    switch (modalType) {
+      case 'card': return <Modal><ModalCard></ModalCard></Modal>
+        break;
+      case 'signIn': return <Modal><FormSignIn></FormSignIn></Modal>
+        break;
+    }
+  }
+  return (
+    <>
+      <Header />
+      {isOpen && showModal()}
+      {props.children}
+      <Footer />
+    </>
+  )
 }
-
 export default WebsiteLayout
