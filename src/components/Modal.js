911c6d6e6c1d76d90cffa79ba3b47232a2664be5
unknown <kavellid63@gmail.com>
2022-10-06 01:02:36 -0300

    agregado de reducer para especificar el contenido del modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,13 +2,14 @@ import { useDispatch } from "react-redux";
 import { closeModal } from "../features/modal/modalSlice";
 import "../styles/Modal.css";
 
-const Modal = () => {
+const Modal = (props) => {
   const dispatch = useDispatch()
   const handleModalContainerClick = (e) => e.stopPropagation();
   return (
     <article className='modal' onClick={() => { dispatch(closeModal()) }}>
       <div className="modal-container" onClick={handleModalContainerClick}>
         <button className="modal-close" onClick={() => { dispatch(closeModal()) }}>x</button>
+        {props.children}
       </div>
     </article>
   )
--- src/components/ModalCard.js
+++ src/components/ModalCard.js
@@ -1,7 +1,3 @@
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Modal from '@mui/material/Modal';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/ModalCard.css'
 const style = {
@@ -19,41 +15,27 @@ const style = {
   p: 4,
 };
 export default function ModalCard() {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
   return (
-    <div>
-      <Button onClick={handleOpen} className="card-add">More INFO</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <div className='modalCard-img-description'>
-            <img className='modalCard-img' src='https://cocinateelmundo.com/wp-content/uploads/2018/06/comida-asiatica-cocinateelmundo-16.jpg'></img>
-            <div className='modalCard-description'>
-              <h2>Description</h2>
-              <span>Serves 2 or 4 people</span>
-              <span> Time</span>
-              <span> Kcal</span>
-            </div>
-          </div>
-          <div className='modalCard-star-ingredients'>
-            <span>⭐⭐⭐</span>
-            <span>Ingredients</span>
-          </div>
-          <div className='modalCard-link-add'>
-            {/* <span>Recipe Link</span> */}
-            <Link to={"/detail"} className="modalCard-recipe">Recipe Link</Link>
-            <button className='modalCard-button'>ADD to Cart</button>
-          </div>
-
-        </Box>
-      </Modal>
-    </div>
+    <>
+      <div className='modalCard-img-description'>
+        <img className='modalCard-img' src='https://cocinateelmundo.com/wp-content/uploads/2018/06/comida-asiatica-cocinateelmundo-16.jpg'></img>
+        <div className='modalCard-description'>
+          <h2>Description</h2>
+          <span>Serves 2 or 4 people</span>
+          <span> Time</span>
+          <span> Kcal</span>
+        </div>
+      </div>
+      <div className='modalCard-star-ingredients'>
+        <span>⭐⭐⭐</span>
+        <span>Ingredients</span>
+      </div>
+      <div className='modalCard-link-add'>
+        {/* <span>Recipe Link</span> */}
+        <Link to={"/detail"} className="modalCard-recipe">Recipe Link</Link>
+        <button className='modalCard-button'>ADD to Cart</button>
+      </div>
+    </>
   );
 }
