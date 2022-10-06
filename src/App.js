b98b371cee60ceaf91a2992a2769b229acd45a8e
unknown <kavellid63@gmail.com>
2022-10-05 22:40:04 -0300

    modal implementado de manera global (en App.js)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import './App.css';
-import WebsiteLayout from './layouts/WebsiteLayout';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import HIW from './pages/HIW';
-import Detail from './pages/Detail';
-import CartBag from './pages/CartBag';
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+
+import WebsiteLayout from './layouts/WebsiteLayout'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import HIW from './pages/HIW'
+import Detail from './pages/Detail'
+import CartBag from './pages/CartBag'
+import Modal from './components/Modal'
 
 function App() {
+  const { isOpen } = useSelector((store) => store.modal)
   return (
     <BrowserRouter>
       <WebsiteLayout>
+        {isOpen && <Modal/>}
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/hiw' element={<HIW />} />
