8a0173c5680fdd8d3fbc9e1a240e646dc596b859
unknown <kavellid63@gmail.com>
2022-10-05 22:41:59 -0300

    modal cambiado a generico, fix en modal.css

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,17 +1,14 @@
+import { useDispatch } from "react-redux";
+import { closeModal } from "../features/modal/modalSlice";
 import "../styles/Modal.css";
 
-const Modal = ({ isOpen, closeModal, text, result }) => {
+const Modal = () => {
+  const dispatch = useDispatch()
   const handleModalContainerClick = (e) => e.stopPropagation();
   return (
-    <article className={`modal ${isOpen && "is-open"}`} onClick={closeModal}>
+    <article className='modal' onClick={() => { dispatch(closeModal()) }}>
       <div className="modal-container" onClick={handleModalContainerClick}>
-        <button className="modal-close" onClick={closeModal}>x</button>
-        {result ?
-          <img className="modal-image" src="https://cdn-icons-png.flaticon.com/512/190/190411.png" alt="ok-icon" />
-          :
-          <img className="modal-image" src="https://cdn-icons-png.flaticon.com/512/6711/6711656.png" alt="ok-icon" />
-        }
-        <p>{text}</p>
+        <button className="modal-close" onClick={() => { dispatch(closeModal()) }}>x</button>
       </div>
     </article>
   )
