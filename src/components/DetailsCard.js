8c6ad3c7ee6c4efd35520d995ba3637f0e3b4b6f
juanPedroGalarza <juanpg232@gmail.com>
2022-10-06 15:30:59 -0300

    fix responsive

--- src/components/DetailsCard.js
+++ src/components/DetailsCard.js
@@ -1,41 +1,30 @@
 import { Link } from 'react-router-dom';
 import '../styles/ModalCard.css'
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  maxWidth: 1200,
-  minWidth: 290,
-  width: '80%',
-  height: '80vh',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+
 export default function ModalCard() {
 
   return (
-    <>
-      <div className='modalCard-img-description'>
-        <img className='modalCard-img' src='https://cocinateelmundo.com/wp-content/uploads/2018/06/comida-asiatica-cocinateelmundo-16.jpg'></img>
-        <div className='modalCard-description'>
+    <div className="modal-card-container">
+      <div className='modal-card-title'>
+        <p>Recipe Name</p>
+      </div>
+      <div className='modal-card-img-container'>
+        <img className='modal-card-img' src='https://cocinateelmundo.com/wp-content/uploads/2018/06/comida-asiatica-cocinateelmundo-16.jpg'></img>
+      </div>
+      <div className='modal-card-description'>
           <h2>Description</h2>
+        <div className='modal-card-sub-description'>
+          <span>⭐⭐⭐</span>
+          <span>Ingredients</span>
           <span>Serves 2 or 4 people</span>
           <span> Time</span>
           <span> Kcal</span>
         </div>
+        <div className='modal-card-link-add'>
+          <Link to={"/detail"} className="modal-card-recipe">Recipe Link</Link>
+          <button className='modal-card-button'>ADD to Cart</button>
+        </div>
       </div>
-      <div className='modalCard-star-ingredients'>
-        <span>⭐⭐⭐</span>
-        <span>Ingredients</span>
-      </div>
-      <div className='modalCard-link-add'>
-        {/* <span>Recipe Link</span> */}
-        <Link to={"/detail"} className="modalCard-recipe">Recipe Link</Link>
-        <button className='modalCard-button'>ADD to Cart</button>
-      </div>
-    </>
+    </div>
   );
 }
