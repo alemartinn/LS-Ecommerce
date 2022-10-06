1f678af66cb473eb194ec26e717d4e32a057e284
unknown <kavellid63@gmail.com>
2022-10-05 22:39:01 -0300

    slice y reducer de modal en store

--- src/features/store.js
+++ src/features/store.js
@@ -1,13 +1,15 @@
 import {configureStore} from "@reduxjs/toolkit"
 import { usersAPI } from "./actions/usersAPI"
 import userReducer from "./user/userSlice"
+import modalReducer from "./modal/modalSlice"
 import colorReducer from "./theme/themeSlice"
 
 export const store = configureStore({
     reducer:{
             [usersAPI.reducerPath]: usersAPI.reducer,
             user: userReducer,
-            color: colorReducer
+            color: colorReducer,
+            modal: modalReducer
         },
         middleware: (getDefaultMiddleware)=> getDefaultMiddleware({
             inmutableCheck:false,
