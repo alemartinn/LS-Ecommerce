b0f3d8a5a702efa9e0a3757ce935fd535305f7ef
unknown <kavellid63@gmail.com>
2022-10-07 06:46:35 -0300

    styles fix

--- src/pages/HIW.js
+++ src/pages/HIW.js
@@ -10,37 +10,42 @@ function HIW() {
     let container = e.currentTarget.parentElement
     var answer = container.children[1].firstChild
     var trigger = container.firstChild.children[1]
-
+    
     if (trigger.classList.contains('hiw-faq-o')) {
-      trigger.classList.remove('hiw-faq-o');
+      trigger.classList.remove('hiw-faq-o')
     }
-    else {
-      trigger.classList.add('hiw-faq-o');
+    else if (!answer.classList.contains('active')) {
+      trigger.classList.add('hiw-faq-o')
     }
     if (container.classList.contains('hiw-faq-expanded')) {
-      container.classList.remove('hiw-faq-expanded');
+      container.classList.remove('hiw-faq-expanded')
     }
-    else {
-      container.classList.add('hiw-faq-expanded');
+    else if (!answer.classList.contains('active')) {
+      container.classList.add('hiw-faq-expanded')
     }
+
     if (!answer.classList.contains('active')) {
-      answer.classList.add('active');
-      answer.style.height = 'auto';
-      let height = answer.clientHeight + "px";
-      answer.style.height = '0px';
+      answer.classList.add('active')
+      answer.style.height = 'auto'
+      let height = answer.clientHeight + "px"
+      answer.style.height = '0px'
       setTimeout(function () {
-        answer.style.height = height;
-      }, 0);
+        answer.style.height = height
+      }, 0)
     } else {
-      answer.style.height = '0px';
+      answer.style.height = '0px'
       answer.addEventListener('transitionend', function () {
-        answer.classList.remove('active');
+        answer.classList.remove('active')
       }, {
         once: true
       });
     }
   }
   const { bcgColor, fontColor, thirdColor, fourthColor, fivethColor } = useSelector(state => state.color)
+  const triggerStyle = {
+    borderLeft: `2px solid ${fontColor}`,
+    borderBottom: `2px solid ${fontColor}`
+  }
   return (
     <main className="hiw-main" style={{ backgroundColor: bcgColor }}>
       <div className="hiw-content">
@@ -78,54 +83,58 @@ function HIW() {
             </div>
           </div>
         </div>
+
         <div className="hiw-promo">
           <img src="https://drive.google.com/uc?export=view&id=1BHTxfI6vBGxWh3w24ePciX2YZqC43dPe"></img>
         </div>
+
         <div className="hiw-benefits"
           style={{ backgroundColor: fourthColor }}>
           <p className="hiw-text"
             style={{ color: fontColor }} >Benefits</p>
         </div>
+
         <div className="hiw-faq"
           style={{ backgroundColor: thirdColor }}>
           <h1 style={{ color: fontColor }}>Frequently Asked Questions</h1>
-          <div className="hiw-faq-topic">
+          <div className="hiw-faq-topic" style={{ borderBottom: `1px solid ${fontColor}` }}>
             <div className="hiw-faq-open" onClick={handleOpenFaq}>
               <h2 className="hiw-faq-question" style={{ color: fontColor }}>1. What's in the box?</h2>
-              <span className="hiw-faq-t"></span>
+              <span className="hiw-faq-t" style={triggerStyle}></span>
             </div>
             <div className="hiw-faq-answer-wrapper">
               <p className="hiw-faq-answer" style={{ color: fontColor }}>All the pre-portioned ingredients to cook the delicious, healthy recipes you've chosen - all our meat  is free-range and ethically sourced from small farms, fresh fish is sustainably caught and our vegetables are cultivated in the most natural way possible.</p>
             </div>
           </div>
-          <div className="hiw-faq-topic">
+          <div className="hiw-faq-topic" style={{ borderBottom: `1px solid ${fontColor}` }}>
             <div className="hiw-faq-open" onClick={handleOpenFaq}>
               <h2 className="hiw-faq-question" style={{ color: fontColor }}>2. How does delivery work?
-              </h2><span className="hiw-faq-t"></span>
+              </h2><span className="hiw-faq-t" style={triggerStyle}></span>
             </div>
             <div className="hiw-faq-answer-wrapper">
               <p className="hiw-faq-answer" style={{ color: fontColor }}>We offer free delivery nationwide. On day of delivery, you'll get a text with an eta but you don't need to be in to receive your box. Our insulated boxes will be keep everything fresh for up to 48 hours and your box will be left in your designated safe place.</p>
             </div>
           </div>
-          <div className="hiw-faq-topic">
+          <div className="hiw-faq-topic" style={{ borderBottom: `1px solid ${fontColor}` }}>
             <div className="hiw-faq-open" onClick={handleOpenFaq}>
               <h2 className="hiw-faq-question" style={{ color: fontColor }}>3. Do you offer a guarantee?
-              </h2><span className="hiw-faq-t"></span>
+              </h2><span className="hiw-faq-t" style={triggerStyle}></span>
             </div>
             <div className="hiw-faq-answer-wrapper">
               <p className="hiw-faq-answer" style={{ color: fontColor }}>If you're not entirely happy, we offer 100% money-back guarantee.</p>
             </div>
           </div>
-          <div className="hiw-faq-topic">
+          <div className="hiw-faq-topic" style={{ borderBottom: `1px solid ${fontColor}` }}>
             <div className="hiw-faq-open" onClick={handleOpenFaq}>
               <h2 className="hiw-faq-question" style={{ color: fontColor }}>4. How many tickles does it take to tickle an octopus?
-              </h2><span className="hiw-faq-t"></span>
+              </h2><span className="hiw-faq-t" style={triggerStyle}></span>
             </div>
             <div className="hiw-faq-answer-wrapper">
               <p className="hiw-faq-answer" style={{ color: fontColor }}>ten tickles</p>
             </div>
           </div>
         </div>
+        
       </div>
     </main>
   )
