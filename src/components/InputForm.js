3fa55db60ee563a9cc8a5e5926a7f1481d5e8db5
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-10-07 16:41:50 -0300

    to fix

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -3,8 +3,16 @@ import '../styles/form/InputForm.css'
 const InputForm = (props) => {
     return (  
         <div className='inputForm-field'>
-            <label className='inputForm-label'>{props.name}</label>
-            <input className='inputForm-input'/>
+            {/* {
+                props.name && <label className='inputForm-label'>{props.name}</label>
+            }
+            <input className='inputForm-input' {...props.input}/> */}
+
+            <label className='inputForm-label' style={{ ...props.style }}>
+                <p>{props.label}</p>
+                <input className='inputForm-input' {...props.input}
+                    style={{ ...props.style }} />
+            </label>
         </div>
     );
 }
