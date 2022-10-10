ffb42d586cc127630e55fb397507ac953139e729
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-10-10 16:21:47 -0300

    Added link to details to cards

--- src/components/CardRecipe.js
+++ src/components/CardRecipe.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { Link as LinkRouter } from 'react-router-dom';
 import '../styles/CardRecipe.css'
 
 const CardRecipe = (props) => {
@@ -8,12 +9,17 @@ const CardRecipe = (props) => {
     return (  
         <div className="card-recipe">
             <div className="card-recipe-info" style={{backgroundColor: bcgColor, color: fontColor}}>
-                <img 
-                    alt='food' 
-                    className='card-recipe-img' 
-                    src='https://img.hellofresh.com/ar_1,w_384,q_auto,f_auto,c_fill,fl_lossy/hellofresh_s3/image/peppercorn-steak-w06-e4014085.jpg'
-                />
-                <p className='card-recipe-name'>{props.title}</p>
+                <LinkRouter to={`/details/${props.id}`} className='card-recipe-link'>
+                    <img 
+                        alt='food' 
+                        className='card-recipe-img' 
+                        src={props.image}
+                        // src='https://img.hellofresh.com/ar_1,w_384,q_auto,f_auto,c_fill,fl_lossy/hellofresh_s3/image/peppercorn-steak-w06-e4014085.jpg'
+                    />
+                </LinkRouter>
+                <div className='card-recipe-container-name'>
+                    <p className='card-recipe-name'>{props.title}</p>
+                </div>
             </div>
         </div>
     );
