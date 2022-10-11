adf1a972ef0a2c473f044ac07262386ba83bc684
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-10-11 18:53:21 -0300

    fixing form

--- src/components/FormRecipes.js
+++ src/components/FormRecipes.js
@@ -6,26 +6,105 @@ import { setCredentials } from "../features/user/userSlice";
 import Button from "./Button";
 import InputForm from "./InputForm";
 import '../styles/FormRecipes.css'
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 const FormRecipes = () => {
+    
     const { fontColor } = useSelector(state=>state.color)
-
+    const [quantityIngredients, setQuantityIngredients] = useState(1);
+    
     const dispatch = useDispatch();
-    const createRecipe = useCreateRecipeMutation();
+    const [createRecipe] = useCreateRecipeMutation();
 
     let modelNewRecipe = [
         {name:'title',label:"recipe name", type: 'text', required: 'required'},
-        {name:'image',label:"image", type: 'text', required: 'required'},
-        {name:'description',label:"description", type: 'text', required: 'required'},
-        {name:'calories',label:"calories", type: 'text', required: 'required'},
-        {name:'ingredients',label:"ingredients", type: 'text', required: 'required'},
-        {name:'preptime',label:"preptime", type: 'number', required: 'required'},
+        {name:'category',label:"category: (Ej: Vegan, High Protein, etc)", type: 'text', required: 'required'},
+        // {name:'ingredients',label:"ingredients", type: 'text', required: 'required'},
+        {name:'preptime',label:"preptime: (Minutes)", type: 'number', required: 'required'},
+        {name:'image',label:"image: (Url)", type: 'text', required: 'required'},
         {name:'allergens',label:"allergens", type: 'text', required: 'required'},
-        {name:'category',label:"category", type: 'text', required: 'required'},
+        {name:'calories',label:"calories (Kcal)", type: 'text', required: 'required'},
+        {name:'description',label:"description", type: 'text', required: 'required'},
     ]
+   
+    const FieldIngredient = () => {
+        return (
+            <fieldset className="field-ingredient-container" style={{
+                color: fontColor,
+                backgroundColor: 'inherit'
+            }}>
+                <input 
+                    className="field-ingredient-input" 
+                    style={{
+                        color: fontColor,
+                        backgroundColor: 'inherit'
+                    }}
+                    placeholder="Ingredient name"
+                />
+                <input 
+                    className="field-ingredient-input" 
+                    style={{
+                        color: fontColor,
+                        backgroundColor: 'inherit'
+                    }} 
+                    placeholder="Ingredient category"
+                />
+                <input 
+                    className="field-ingredient-input" 
+                    style={{
+                        color: fontColor,
+                        backgroundColor: 'inherit'
+                    }} 
+                    placeholder="Quantity"
+                />
+                <select 
+                    className="field-ingredient-select"
+                    style={{
+                        color: fontColor,
+                        backgroundColor: 'inherit'
+                    }} 
+                >
+                    <option 
+                        className="field-ingredient-option" 
+                        style={{
+                        color: fontColor,
+                        backgroundColor: 'inherit'
+                        }}  
+                        value="Units"
+                    >
+                        Units
+                    </option>
+                    <option 
+                        className="field-ingredient-option"
+                        style={{
+                            color: fontColor,
+                            backgroundColor: 'inherit'
+                        }}  
+                        value="gr"
+                    >
+                        Gr
+                    </option>
+                </select>
+            </fieldset>
+        )
+    }
+
+    const addFieldIngredient = () => {
+        setQuantityIngredients(quantityIngredients+1)
+    }
+
+    const showAllIngredientsFields = (quantityIngredients) => {
+
+        return(
+            Array.from({length: quantityIngredients}, () => <FieldIngredient />)
+        )
+        
+    }
 
     const handleSubmit = (e) => {
-        e.prevent.default();
+        e.preventDefault();
         let inputs = Array.from(e.target);
         inputs= inputs.filter((input) => input.name);
 
@@ -33,17 +112,31 @@ const FormRecipes = () => {
             data[inputValue.name] = inputValue.value
             return data
         },{});
+        console.log(dataInputs)
 
-        createRecipe(dataInputs).unwrap().then(res => {
+        let recipe = {
+            title: dataInputs.title,
+            category: dataInputs.category,
+            image: dataInputs.image,
+            calories: dataInputs.calories,
+            preptime: dataInputs.preptime,
+            // ingredients: [{name: dataInputs.ingredients, category: dataInputs.category, quantity: '200gr'}],
+            allergens: [dataInputs.allergens],
+            description: dataInputs.description
+        }
+
+        console.log(recipe);
+        createRecipe(recipe).unwrap().then(res => {
             if (res.success) {
                 dispatch(setCredentials(res.response))
-                dispatch(closeModal())
             }
-            dispatch(specifyMessage(res.message))
-            dispatch(openAlert(res.success))
+            console.log(res)
+            // dispatch(specifyMessage(res.message))
+            // dispatch(openAlert(res.success))
         }).catch(err => {
-            dispatch(specifyMessage(err.data.message))
-            dispatch(openAlert(false))
+            console.log(err.data.message.details[0].message)
+            // dispatch(specifyMessage(err.data.message))
+            // dispatch(openAlert(false))
         })
     }
 
@@ -59,11 +152,33 @@ const FormRecipes = () => {
                         key:atrib.name,
                     }}
                     style={{
-                        color: fontColor
+                        color: fontColor,
+                        backgroundColor: 'inherit'
                     }}
                     key={atrib.name}
                 />
             )}
+            <div className="form-recipes-container-ingredients">
+                <div>
+                    <span>Ingredients: (Ex: Tomato - Vegetal - 2 - Units)</span>
+                    <button type="button" onClick={addFieldIngredient} className="add-field-ingredient" style={{backgroundColor: 'inherit', color: fontColor}}>
+                        Add <FontAwesomeIcon icon={faPlus}/>
+                    </button>
+                </div>
+                { showAllIngredientsFields(quantityIngredients) }
+            </div>
+
+            {/* <textarea 
+                style={{
+                    color: fontColor,
+                    backgroundColor: 'inherit'
+                }}
+                id="descriptionText" 
+                className="formRecipes-text-area" 
+                name="descriptionText" 
+                placeholder="A description of the recipe is required" 
+                required 
+            /> */}
             <Button>
                 Send
             </Button>
