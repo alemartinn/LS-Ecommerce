e8527110f164e11be5a794f66b97751f3eae79d1
Elvio Galeano <elvio.galeano827@gmail.com>
2022-10-11 02:35:38 -0300

    dia normal

--- src/components/dashboard/CreateRecipe.js
+++ src/components/dashboard/CreateRecipe.js
@@ -1,7 +1,37 @@
+import { useCreateRecipeMutation } from '../../features/recipes/recipesApi'
+import '../../styles/CreateRecipe.css'
 export default function CreateRecipe() {
-    return (
-        <div>
-            <p>Create a recipe...</p>
+
+    const arrayInput = [
+        {name:"Title"},
+        {name:"Image"},
+        {name:"Description"},
+        {name:"Calories"},
+        {name:"Ingredients"},
+        {name:"Preptime"},
+        {name:"Allergens"},
+        {name:"Category"}
+    ]
+    const [newRecipe] = useCreateRecipeMutation()
+    const inputs = (item) => (
+        <div key={item.name}>
+            <input  className="input-name" placeholder={item.name} />
+        </div>  
+    )
+const create = ()=>{
+ 
+}
+return (
+        <div className="recipe-container">
+            <div className="recipe-container-main">
+                <h2>Create Recipe</h2>
+                <form>
+                    {arrayInput?.map(inputs)}
+                    <div className='recipe-button'>
+                        <button className='recipe-button-send' onClick={create}>Send</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
