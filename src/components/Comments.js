71aa79eea42bb40cccb5a4ed8d4b02494882ce54
Alejandro Condori <Ale_xeg147@hotmail.com>
2022-10-14 06:53:17 -0300

    comment component created

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -5,68 +5,73 @@ import '../styles/Comments.css'
 
 
 export default function Comments({id}) {
-const divText = useRef(null)
-const [showComment,setShowCommment] = useState(false)
-const {data} = useAllCommentQuery(id)
-const [comment,setComment] = useState()
-const { bcgColor, fontColor } = useSelector(state => state.color);
-useEffect(()=>{
-    if(data){
-        console.log(data.response)
-        setComment(data.response)
-    }
-},[data])
-const printComment = (item)=>(
-    <div className='comment-container' key={item._id}>
-        <div className='comment-profile' >
-            <img className='comment-img' src={item.user.photo}/>
-            <h2>{item.user.name}</h2>
-        </div>
-        <div className='comment-message' >
-            <p className='comment-message-p'>{item.message}</p>
-        </div>
-    </div>
-)
-const click = ()=>{
-    setShowCommment(!showComment)
-}
 
+    const [showComment,setShowCommment] = useState(false);
+    const [comment,setComment] = useState();
+    const [newcom] = useCreateMutation();
+    
+    const { bcgColor, fontColor } = useSelector(state => state.color);
+    const { user } = useSelector(state=>state.user);
+    const {data} = useAllCommentQuery(id);
+    const divText = useRef(null);
+    
+    useEffect(()=>{
+        if(data){
+            console.log(data.response)
+            setComment(data.response)
+        }
+    },[data]);
 
-    // NEW COMMENT
-    const [newcom] = useCreateMutation()
-    const { user } = useSelector(state=>state.user)
+    const printComment = (item)=>(
+        <div className='comment-container' key={item._id}>
+            <div className='comment-profile' >
+                <div>
+                    <img className='comment-img' src={item.user.photo} alt={'userPhoto'}/>
+                </div>
+                <h2>{item.user.name}</h2>
+            </div>
+            <div className='comment-message' >
+                <p className='comment-message-p'>{item.message}</p>
+            </div>
+        </div>
+    );
+
+    const click = ()=>{
+        setShowCommment(!showComment)
+    };
+
     const sendComment = async()=>{
         let body ={message:divText.current.value,
             recipe:id,
             user: user.id}
             await newcom(body)
-    }
+    };
 
 
   return ( 
-    <div className='comment-main'>
-        {/* <h2 className='comment-title'>Comments</h2> */}
+    <div className='comment-main' style={{backgroundColor: bcgColor, color: fontColor}}>
+        <h2 className='comment-title'>Comments: ({comment?.length})</h2>
         <div className='btn'>
-            <button className='btn-main ' onClick={()=>click()}>Comments</button>
+            {
+                comment && <button className='btn-main ' onClick={()=>click()}>Load comments </button>
+            }
         </div>
-        <div className='comment-boxcomment'>
-            {showComment ? 
-            <div>
+        {
+            showComment 
+            ?  
+            <div className='comment-boxcomment'>
                 { comment?.map(item =>printComment(item))}
-                {/* NEW COMMENT  */}
-                <div className='comment-createcomment'>
-                    <div className='btn'>
-
-                    <button className='btn-main'  type='submit' onClick={()=>sendComment()}>Post comment</button>
-                    </div>
-                    <input className='inputForm-input'  type="text"  ref={divText}></input>
-                    
-                </div>
-            </div>  : null }
+                
+            </div>  
+            : 
+            null 
+        }
+        <div className='comment-createcomment'>
+            <div className='btn'>
+                <button className='btn-main' type='submit' onClick={()=>sendComment()}>Post comment</button>
+            </div>
+            <input className='inputForm-input' type="text" ref={divText} style={{backgroundColor: bcgColor, color: fontColor}} placeholder={'Leave your comment here!'}></input>
         </div>
-        {/* <NewComment getAllComment={getAllComment} id={id}/> */}
-            
-        
     </div>
   )
 }
